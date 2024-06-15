27ff9c6036f6e02cc2b885ecaaf01ae685047029
Bogdan Marchulov <bogdan_info@mail.ru>
2024-06-15 21:31:38 +0300

    feat: add static

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,20 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_files::Files;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(Files::new("/", "../../../client/build").index_file("index.html"))
+            .default_service(
+                web::route().to(|| HttpResponse::NotFound())
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
