46c75e1d357dbd57eb1a8630938e100a3e28c182
Bogdan Marchulov <bogdan_info@mail.ru>
2024-06-16 00:00:29 +0300

    find static

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Result};
-use include_dir::{Dir, include_dir as include_d};
-use actix_files as fs;
+use actix_files::NamedFile;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use include_dir::{include_dir as include_d, Dir};
+use std::path::PathBuf;
 
 const FRONTEND_DIR: Dir = include_d!("../client/build");
 
@@ -11,15 +12,23 @@ async fn index(_req: HttpRequest) -> HttpResponse {
         .body(file.contents())
 }
 
+async fn static_files(req: HttpRequest) -> HttpResponse {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let file = FRONTEND_DIR.get_file(&path).unwrap();
+    println!("{}", &path.to_str().unwrap());
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(file.contents())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .service(fs::Files::new("/static", ".").show_files_listing())
+            .route("/{filename:.*}", web::get().to(static_files))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
-
