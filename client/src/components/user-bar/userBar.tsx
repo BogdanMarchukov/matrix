5dbfec84608ecfe7e74d14ff2f17b347cb8c1ed3
Bogdan Marchulov <bogdan_info@mail.ru>
2024-08-15 22:11:40 +0300

    feat: add loader

--- client/src/components/user-bar/userBar.tsx
+++ client/src/components/user-bar/userBar.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Puff, ThreeDots } from "react-loader-spinner";
 import { useLogin } from "../../common/hooks/useLogin";
 import useTelegramInitData from "../../common/hooks/useTelegramInitData";
 import Card from "./svg/card";
@@ -9,6 +10,12 @@ import classes from "./userBar.module.css";
 function UserBar() {
   const init = useTelegramInitData();
   const { login, loading, error, data } = useLogin();
+  const firstName =
+    data?.auth?.login?.user?.firstName ||
+    data?.auth?.login?.user?.lastName ||
+    "имя ";
+  const avatarUrl = data?.auth?.login?.user?.photoUrl || firstName[0];
+  const loaderConfig = { height: 20, width: 30, color: "#ffffff	" };
 
   useEffect(() => {
     login();
@@ -17,10 +24,18 @@ function UserBar() {
   return (
     <div className={classes.container}>
       <div className={classes.leftItem}>
-        <div className={classes.avatar}></div>
+        <div className={classes.avatar}>
+          {loading ? (
+            <Puff {...loaderConfig} height={24} width={24} />
+          ) : (
+            avatarUrl
+          )}
+        </div>
         <div>
           <div>
-            <div className={classes.username}>{data?.auth?.login?.jwt}</div>
+            <div className={classes.username}>
+              {loading ? <ThreeDots {...loaderConfig} /> : firstName}
+            </div>
           </div>
           <div className={classes.userStatus}>
             <span>Новичок</span>
