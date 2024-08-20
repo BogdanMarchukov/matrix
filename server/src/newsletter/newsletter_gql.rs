a6df3287d34c28052088427df4ea8f3118b7b10d
Bogdan Marchulov <bogdan_info@mail.ru>
2024-08-20 21:38:22 +0300

    feat: input

--- server/src/newsletter/newsletter_gql.rs
+++ server/src/newsletter/newsletter_gql.rs
@@ -1,10 +1,23 @@
-use async_graphql::{Context, FieldResult, Object};
+use async_graphql::{Context, FieldResult, InputObject, Object};
+use chrono::{DateTime, Utc};
 
 pub struct NewsletterMutation;
 
+#[derive(InputObject)]
+pub struct NewsLetterCreateInput {
+    pub title: String,
+    pub is_published: bool,
+    pub payload: String,
+    pub publish_at: DateTime<Utc>,
+}
+
 #[Object]
 impl NewsletterMutation {
-    async fn create_one<'ctx>(&self, ctx: &Context<'ctx>) -> FieldResult<bool> {
+    async fn create_one<'ctx>(
+        &self,
+        ctx: &Context<'ctx>,
+        data: NewsLetterCreateInput,
+    ) -> FieldResult<bool> {
         Ok(true)
     }
 }
