8010b73cf5e5ab6a6b6baae3ab35a95e22ced3b5
bogdan <bogdan@MacBook-Pro-bogdan.local>
2025-06-13 17:48:16 +0300

    fix: codestyle

--- server/src/common/guards/http_system_guard.rs
+++ server/src/common/guards/http_system_guard.rs
@@ -6,13 +6,7 @@ pub fn verify_api_key(ctx: &GuardContext) -> bool {
     let auth_header = ctx.head().headers().get("x-api-key");
     let api_key = config::get_api_key();
     match auth_header {
-        Some(auth) => {
-            if auth == &api_key {
-                true
-            } else {
-                false
-            }
-        }
+        Some(auth) => auth == &api_key,
         None => false,
     }
 }
