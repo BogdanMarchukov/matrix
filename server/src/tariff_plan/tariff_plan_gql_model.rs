b1dc82e3ee577c44674d1e735dccf7c0a5a6877c
bogdan <bogdan@MacBook-Pro-bogdan.local>
2025-06-18 18:27:22 +0300

    feat: parce price

--- server/src/tariff_plan/tariff_plan_gql_model.rs
+++ server/src/tariff_plan/tariff_plan_gql_model.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 
 use async_graphql::*;
 
-use crate::entity::tariff_plan;
+use crate::{entity::tariff_plan, errors::gql_error::GqlError};
 
 #[derive(Clone, SimpleObject)]
 #[graphql(name = "TariffPlan")]
@@ -17,13 +17,17 @@ pub struct TariffPlanGqlModel {
 }
 
 impl TariffPlanGqlModel {
-    pub fn new(tariff_plan: tariff_plan::Model) -> Self {
-        Self {
-            tariff_plan_id: tariff_plan.tariff_plan_id,
-            title: tariff_plan.title,
-            description: tariff_plan.description,
-            price: tariff_plan.price.to_f64().unwrap_or(0.00),
-            expiry_days: tariff_plan.expiry_days,
+    pub fn new(tariff_plan: tariff_plan::Model) -> FieldResult<Self> {
+        if let Some(price) = tariff_plan.price.to_f64() {
+            Ok(Self {
+                tariff_plan_id: tariff_plan.tariff_plan_id,
+                title: tariff_plan.title,
+                description: tariff_plan.description,
+                price,
+                expiry_days: tariff_plan.expiry_days,
+            })
+        } else {
+            Err(GqlError::ServerError("Parse price error".to_string()).extend())
         }
     }
 }
