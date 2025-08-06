9707accb64ed408971df021b54458be05769e8be
Your Name <you@example.com>
2025-08-06 15:28:48 +0300

    refactor: обновить модель OfferLike для соответствия OfferGqlModel

--- server/src/offer_like/offer_like_model.rs
+++ server/src/offer_like/offer_like_model.rs
@@ -1,28 +1,11 @@
-use crate::entity::offer_like;
-use sea_orm::prelude::*;
 
-pub struct OfferLikeModel;
+// сделай как в OfferGqlModel AI!
 
-impl OfferLikeModel {
-    pub async fn find_by_id(
-        db: &DatabaseConnection,
-        id: Uuid,
-    ) -> Result<Option<offer_like::Model>, DbErr> {
-        offer_like::Entity::find_by_id(id).one(db).await
-    }
-
-    pub async fn create(
-        db: &DatabaseConnection,
-        offer_id: Uuid,
-        user_id: Uuid,
-    ) -> Result<offer_like::Model, DbErr> {
-        let new_offer_like = offer_like::ActiveModel {
-            offer_like_id: Set(Uuid::new_v4()),
-            offer_id: Set(offer_id),
-            user_id: Set(user_id),
-            ..Default::default()
-        };
-        let res = offer_like::Entity::insert(new_offer_like).exec(db).await?;
-        offer_like::Entity::find_by_id(res.last_insert_id).one(db).await
-    }
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "offer_like")]
+pub struct OfferLikeGqlModel {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub offer_like_id: Uuid,
+    pub offer_id: Uuid,
+    pub user_id: Uuid,
 }
