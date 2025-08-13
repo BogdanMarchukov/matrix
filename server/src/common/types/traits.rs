6e0165ffc640a76d5352bf8c00f73b96f53be5b4
bogdan <marchukovb@gmail.com>
2025-08-13 23:48:55 +0300

    feat: add method for repository

--- server/src/common/types/traits.rs
+++ server/src/common/types/traits.rs
@@ -1,12 +1,23 @@
 use sea_orm::{ConnectionTrait, DbErr, ModelTrait};
+use uuid::Uuid;
 
-trait Repository {
-    async fn find_by_pk<T, C>(id: String, db: &C) -> Result<T, DbErr>
-    where
-        T: ModelTrait,
-        C: ConnectionTrait;
+pub trait OptionFieldsFilter {}
 
-    async fn find_many<T, C, F>(filter: F, db: &C) -> Result<Vec<T>, DbErr>
-    where
-        T: ModelTrait;
+pub trait Repository<T, C, F>
+where
+    T: ModelTrait,
+    C: ConnectionTrait,
+    F: OptionFieldsFilter,
+{
+    async fn find_by_pk(id: Uuid, db: &C) -> Result<T, DbErr>;
+
+    async fn find_many(filter: F, db: &C) -> Result<Vec<T>, DbErr>;
+
+    async fn find_one(filter: F, db: &C) -> Result<Option<T>, DbErr>;
+
+    async fn create_one(data: T, db: &C) -> Result<T, DbErr>;
+
+    async fn update_one(id: Uuid, data: T, db: &C) -> Result<T, DbErr>;
+
+    async fn delete_one(id: Uuid, db: &C) -> Result<bool, DbErr>;
 }
