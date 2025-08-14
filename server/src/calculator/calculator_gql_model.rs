ff4b11e8f34577e91de22b4f0d4b292ee56843cb
bogdan <marchukovb@gmail.com>
2025-08-14 10:52:06 +0300

    feat: add calculatorGlqModel

---
+++ server/src/calculator/calculator_gql_model.rs
@@ -0,0 +1,30 @@
+use crate::entity::calculator;
+use crate::entity::sea_orm_active_enums::CalculatorType;
+use async_graphql::{Enum, SimpleObject};
+use uuid::Uuid;
+
+#[derive(Clone, Debug, SimpleObject)]
+#[graphql(name = "Calculator")]
+pub struct CalculatorGqlModel {
+    pub calculator_id: Uuid,
+    pub r#type: CalculatorGqlType,
+    pub require_params: Option<Vec<String>>,
+    pub options_params: Option<Vec<String>>,
+}
+
+#[derive(Enum, Copy, Clone, Eq, PartialEq, Debug)]
+#[graphql(remote = "CalculatorType", name = "CalculatorType")]
+pub enum CalculatorGqlType {
+    MatrixSchema,
+}
+
+impl From<calculator::Model> for CalculatorGqlModel {
+    fn from(model: calculator::Model) -> Self {
+        Self {
+            calculator_id: model.calculator_id,
+            r#type: model.r#type.into(),
+            require_params: model.require_params,
+            options_params: model.options_params,
+        }
+    }
+}
--- server/src/calculator/mod.rs
+++ server/src/calculator/mod.rs
@@ -1,3 +1,4 @@
 
 pub mod calculator_repository;
+pub mod calculator_gql_model;
 
